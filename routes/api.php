d0ea44f2dc849f350b8835fc4a0f60be3cc18005
Ali Kamal <alikamal1@yahoo.com>
2018-10-24 15:17:56 +0300

    Password_Reset

--- routes/api.php
+++ routes/api.php
@@ -27,3 +27,12 @@ Route::group(['prefix' => 'auth'], function () {
         Route::get('user', 'AuthController@user');
     });
 });
+
+Route::group([
+    'middleware' => 'api',
+    'prefix' => 'password'], 
+    function () {
+        Route::post('create','PasswordResetController@create');
+        Route::get('find/{token}', 'PasswordResetController@find');
+        Route::post('reset', 'PasswordResetController@reset');
+});
