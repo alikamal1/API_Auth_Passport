55b8dc1d870b0afc6a5607dbe760bb9cf0f32aa9
Ali Kamal <alikamal1@yahoo.com>
2018-10-24 13:03:58 +0300

    API_Rest_with_Passport

--- routes/api.php
+++ routes/api.php
@@ -20,9 +20,9 @@ use Illuminate\Http\Request;
 Route::group(['prefix' => 'auth'], function () {
     Route::post('login', 'AuthController@login');
     Route::post('signup', 'AuthController@signup');
-});
 
-Route::group(['middleware' => ['auth:api']], function () {
-    Route::get('logout', 'AuthController@logout');
-    Route::get('user', 'AuthController@user');
+    Route::group(['middleware' => ['auth:api']], function () {
+        Route::get('logout', 'AuthController@logout');
+        Route::get('user', 'AuthController@user');
+    });
 });
